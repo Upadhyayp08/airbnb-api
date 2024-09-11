53d37416ab07e2dd429e0af5091cc10a83978067
Parth Upadhyay <upadhyayparth080@gmail.com>
2024-09-11 16:38:03 +0530

    file upload
    file upload

--- main.go
+++ main.go
@@ -11,18 +11,19 @@ import (
 )
 
 func main() {
-	// Initialize MongoDB connection
-	// Load the environment variables from the .env file
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	database.ConnectDB()
+    // Load environment variables from .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	// Initialize Router
-	r := router.InitializeRouter()
+    // Connect to MongoDB
+    database.ConnectDB()
 
-	// Start the server
-	fmt.Println("Server running on port 8000")
-	log.Fatal(http.ListenAndServe(":8000", r))
+    // Initialize Router
+    r := router.InitializeRouter()
+
+    // Start the server
+    fmt.Println("Server running on port 8000")
+    log.Fatal(http.ListenAndServe(":8000", r))
 }
